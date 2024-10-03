07a2c8a4db7e124a71ec4f259c0f6092a15a3c03
ShimadaNanaki <110211701+ShimadaNanaki@users.noreply.github.com>
2024-10-03 23:17:22 +0900

    ΘÇúµèòπüòπéîπüªπééσ¢░πéïπü«πüº∩╝òσêåπüöπü¿πü½πéñπâÖπâ│πâêπéÆΘÇüΣ┐í

--- cogs/main.py
+++ cogs/main.py
@@ -13,7 +13,7 @@ class Main(commands.Cog):
         self.size = 4000
         self.server_event = []
         self.dead_list = []
-        
+
     def dump_config(self):
         with open("./config.json", "w", encoding="utf-8") as f:
             json.dump(self.config_json, f, indent=2)
@@ -30,8 +30,9 @@ class Main(commands.Cog):
             #定期リスナーを実行します。
             self.refresh_message.start()
             self.event_listener.start()
+            self.server_event_listener.start()
         else: print("Failed Server Connection")
-        
+
     @app_commands.command(name="setup_channel", description="チャンネルを設定します")
     async def setup_channel(self, interaction: Interaction):
         await interaction.response.send_message(f"チャンネルを{interaction.channel.name}に設定しました", ephemeral=True)
@@ -39,7 +40,7 @@ class Main(commands.Cog):
         self.dump_config()
         self.channel_id = interaction.channel_id
         self.channel_message = await self.get_channel_message()
-        
+
     @app_commands.command(name="setup_talk_channel", description="トークチャンネルを設定します")
     async def setup_talk_channel(self, interaction: Interaction):
         await interaction.response.send_message(f"トークチャンネルを{interaction.channel.name}に設定しました", ephemeral=True)
@@ -54,7 +55,7 @@ class Main(commands.Cog):
         #DiscordChatに書かれた内容をチームチャットに送信します。
         if int(message.channel.id) == self.talk_channel_id:
             await self.rust_client.send_team_chat(f"[DISCORD] {message.author.global_name}: {message.content}")
-    
+
     async def get_channel_message(self):
         channel = self.bot.get_channel(self.channel_id)
         messages = channel.history(limit=100)
@@ -63,7 +64,7 @@ class Main(commands.Cog):
                 return message
         message = await channel.send("Not Found Message")
         return message
-    
+
     @tasks.loop(seconds=1)
     async def event_listener(self):
         try:
@@ -79,8 +80,8 @@ class Main(commands.Cog):
                     if member.steam_id not in self.dead_list:
                         self.dead_list.append(member.steam_id)
                         grid = await self.getGrid(member.x, member.y)
-                        await talk_channel.send(f"{member.name} is dead ({grid})")
-                        await self.rust_client.send_team_chat(f"[RUSTBOT] {member.name} is dead ({grid})")
+                        await talk_channel.send(f"{member.name} がしぼうしました。 ({grid})")
+                        await self.rust_client.send_team_chat(f"[RUSTBOT] {member.name} がしぼうしました。 ({grid})")
                 else:
                     if member.steam_id in self.dead_list:
                         self.dead_list.remove(member.steam_id)
@@ -89,35 +90,16 @@ class Main(commands.Cog):
 
     @tasks.loop(seconds=10)
     async def refresh_message(self):
-        try:            
+        try:
             #10秒おきにデータを取得しEmbedを更新します。
             embed = await self.get_embed()
             await self.channel_message.edit(content="", embed=embed)
         except Exception as e: print("Return Error: "+str(e))
 
-    async def get_server_data(self):
-        data = {}
+    @tasks.loop(minutes=5)
+    async def server_event_listener(self):
         try:
-            server_info = await self.rust_client.get_server_info()
-            server_time = await self.rust_client.get_server_time()
-            team_info = await self.rust_client.get_team_info()
-            
-            #サーバーMapサイズを取得します(Get mapsize)
-            self.size = server_info.size
-            
-            #チーム情報を取得します(Get team info)
-            online_member = []
-            offline_member = []
-            team_leader = None
-            for member in team_info.members:
-                if member.steam_id == team_info.leader_steam_id:
-                    team_leader = member.name
-                if member.is_online:
-                    online_member.append(f"{member.name}({await self.getGrid(member.x, member.y)})")
-                else:
-                    offline_member.append(f"{member.name}({await self.getGrid(member.x, member.y)})")
-                    
-            #サーバーイベントを取得します(Get Server event)
+            talk_channel = self.bot.get_channel(self.talk_channel_id)
             server_markers = await self.rust_client.get_server_markers()
             server_events = []
             for marker in server_markers:
@@ -131,7 +113,35 @@ class Main(commands.Cog):
                     server_events.append(f"Crate({await self.getGrid(marker.x, marker.y)})")
                 elif marker.type == 8:
                     server_events.append(f"PatrolHelicopter({await self.getGrid(marker.x, marker.y)})")
-                    
+            for event in server_events:
+                if event not in self.server_event:
+                    await talk_channel.send(f"新しいイベント: {event}")
+            self.server_event = server_events
+        except Exception as e:
+            print("Return Error: " + str(e))
+
+    async def get_server_data(self):
+        data = {}
+        try:
+            server_info = await self.rust_client.get_server_info()
+            server_time = await self.rust_client.get_server_time()
+            team_info = await self.rust_client.get_team_info()
+
+            #サーバーMapサイズを取得します(Get mapsize)
+            self.size = server_info.size
+
+            #チーム情報を取得します(Get team info)
+            online_member = []
+            offline_member = []
+            team_leader = None
+            for member in team_info.members:
+                if member.steam_id == team_info.leader_steam_id:
+                    team_leader = member.name
+                if member.is_online:
+                    online_member.append(f"{member.name}({await self.getGrid(member.x, member.y)})")
+                else:
+                    offline_member.append(f"{member.name}({await self.getGrid(member.x, member.y)})")
+
             data["server_name"] = server_info.name
             data["server_players"] = str(server_info.players)+"/"+str(server_info.max_players)+"("+str(server_info.queued_players)+")"
             data["server_time"] = server_time.time
@@ -139,8 +149,7 @@ class Main(commands.Cog):
             data["team_leader"] = team_leader
             data["online_member"] = online_member
             data["offline_member"] = offline_member
-            data["server_events"] = server_events
-        except: return 
+        except: return
         return data
 
     async def getGrid(self, loc_x, loc_y):
@@ -168,20 +177,15 @@ class Main(commands.Cog):
             embed.add_field(name="プレイヤー数 現在/最大(待機)", value=data["server_players"],inline=False)
             embed.add_field(name="サーバー内時間",value=data["server_time"],inline=True)
             embed.add_field(name="日の出 - 日没",value=data["server_sun_time"],inline=True)
-            embed.add_field(name="サーバーイベント", value=', '.join(map(str, data["server_events"])), inline=False)
             embed.add_field(name="チームリーダー", value=data["team_leader"], inline=False)
             embed.add_field(name="オンライン", value=', '.join(map(str, data["online_member"])), inline=False)
             embed.add_field(name="オフライン", value=', '.join(map(str, data["offline_member"])), inline=False)
             embed.add_field(name="更新時間", value=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), inline=False)
-            for event in data["server_events"]:
-                if event not in self.server_event:
-                    await talk_channel.send(f"新しいイベント: {event}")
-            self.server_event = data["server_events"]
             return embed
         else:
             self.retry_count += 1
             embed = Embed(title=f"接続に失敗しました。({self.retry_count} 回目)",description="")
             return embed
 
-async def setup(bot):    
+async def setup(bot):
     await bot.add_cog(Main(bot))
